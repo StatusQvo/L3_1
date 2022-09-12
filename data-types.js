588bcca9d6c7b6c48f40172468d659d3a19735d9
Darya_Savitskaya <statusqvogerz@gmail.com>
2022-09-12 21:33:12 +0500

    L3_T1

---
+++ data-types.js
@@ -0,0 +1,17 @@
+const myNumber = 153.2887
+const myString = `It's string with number: ${myNumber}`
+const myBigInt = 1n
+const myBoolean = true
+const myNull = null
+const myUnknown = undefined
+const mySymbol = Symbol('someData')
+const myObject = {}
+
+console.log(typeof myNumber)
+console.log(typeof myString)
+console.log(typeof myBigInt)
+console.log(typeof myBoolean)
+console.log("it's null:", typeof myNull)
+console.log(typeof myUnknown)
+console.log(typeof mySymbol)
+console.log(typeof myObject)
